4dadd1e0e19f026a12552c95a36387ea0ef71e31
francesco agati <francescoagati1975@gmail.com>
2019-03-10 01:53:07 +0100

    cleaning

--- src/Main.hx
+++ src/Main.hx
@@ -1,27 +1,25 @@
 import Faker.make;
 
-typedef Tp1 = {x:Int,f:String,tp2:Tp2}
-typedef Tp2 =  {y:String}
-
+typedef Tp1 = {x:Int, f:String, tp2:Tp2}
+typedef Tp2 = {y:String}
 
 typedef A = {
-    a:Int,
-    b:String,
-    c:Float,
-    d:Bool,
+	a:Int,
+	b:String,
+	c:Float,
+	d:Bool,
 	tp1:Tp1,
 	atp1:Array<Tp1>,
-    e:Array<Int>,
-    f:{
-        g:Array<Bool>,
-        ?h:String,
-    },
-    ?i:Int,
+	e:Array<Int>,
+	f:{
+		g:Array<Bool>, ?h:String,
+	},
+	?i:Int,
 }
 
 class Main {
-    static function main() {
-        var tower = make(A);
-        trace(haxe.Json.stringify(tower,null," "));
-    }
+	static function main() {
+		var tower = make(A);
+		trace(haxe.Json.stringify(tower, null, " "));
+	}
 }
